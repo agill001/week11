5bd90ce4d7f2e9fcba25b6239b80218fd41afef6
Aarnav Gill <gill.aarnav@gmail.com>
2022-11-16 12:42:27 -0800

    function dec practice

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+// function greet (user){
+    
+//     console.log("hello " +user + " how are you?");
+// }
+
+const greet = (user)=>{
+    
+         console.log("hello " +user + " how are you?");
+     }
+
+// let userName = prompt("who are you?")
+// greet(userName);
+
+greet("aarnav");

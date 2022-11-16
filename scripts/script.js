10ec50d96ecf50e9ba96bbd3c840d926a850b599
Aarnav Gill <gill.aarnav@gmail.com>
2022-11-16 13:39:54 -0800

    second js practice

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,33 @@
-// function greet (user){
+//  function greet (user,time="day"){
     
-//     console.log("hello " +user + " how are you?");
-// }
+    
+//    console.log("good " +time+user + " how are you?");
+//  }
 
-const greet = (user)=>{
+
+// instead of function u can use =>
+// const greet = (user)=>{
     
-         console.log("hello " +user + " how are you?");
-     }
+//          console.log("hello " +user + " how are you?");
+//      }
 
 // let userName = prompt("who are you?")
 // greet(userName);
 
-greet("aarnav");
+// greet(" aarnav "," night ");
+
+
+
+// function circlecalculate(radius){
+//    let result = Math.PI*radius*radius
+//     return result 
+//  }
+
+// let r = prompt("enter radius");
+// let calcarea = circlecalculate(r).toFixed(2);
+//  alert ("the area of a circle with radius "+r+" is "+calcarea)
+ 
+//  let radiusParagraph = document.querySelector("#radius");
+//  radiusParagraph.textContent +=r;
+//  let areaParagraph = document.querySelector("#result");
+//  areaParagraph.textContent += calculatedArea
